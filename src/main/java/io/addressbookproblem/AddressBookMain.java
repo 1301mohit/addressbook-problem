ae0934f8b795198a8cfd095785649fc10bf9003a
mohit <mohitkr1301>
2021-03-27 08:23:15 +0530

    Added welcome message

---
+++ src/main/java/io/addressbookproblem/AddressBookMain.java
@@ -0,0 +1,13 @@
+package io.addressbookproblem;
+
+/**
+ * Hello world!
+ *
+ */
+public class AddressBookMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Address Book Program" );
+    }
+}
