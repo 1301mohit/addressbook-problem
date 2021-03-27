d803e078aedf481a785149a246a1d05eefd9a160
mohit <mohitkr1301>
2021-03-27 08:57:09 +0530

    Modified: Create object of addresbook class and add contacts

--- src/main/java/io/addressbookproblem/AddressBookMain.java
+++ src/main/java/io/addressbookproblem/AddressBookMain.java
@@ -1,13 +1,18 @@
 package io.addressbookproblem;
 
-/**
- * Hello world!
- *
- */
+
 public class AddressBookMain 
 {
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Address Book Program" );
+        Contact contact1 = new Contact("Mohit", "Kumar", "Mumbai", "Mumbai",
+			       					  "Maharastra", "123456", "9999999999", "mohit@gmail.com");
+        Contact contact2 = new Contact("Amit", "Kumar", "Mumbai", "Mumbai",
+					  				  "Maharastra", "123456", "9999999999", "amit@gmail.com");
+        AddressBook addressBook = new AddressBook();
+        addressBook.createContact(contact1);
+        addressBook.createContact(contact2);
+        System.out.println(addressBook.getListOfContacts());
     }
 }
