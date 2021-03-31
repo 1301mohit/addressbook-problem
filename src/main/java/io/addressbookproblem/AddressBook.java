cb2daf0ea08c38c67af5304894c95b63a9289e7b
mohit <mohitkr1301>
2021-03-31 08:07:16 +0530

    Refactored: create a contacts in addressbook

--- src/main/java/io/addressbookproblem/AddressBook.java
+++ src/main/java/io/addressbookproblem/AddressBook.java
@@ -5,15 +5,14 @@ import java.util.List;
 
 public class AddressBook {
 	
-	private List<Contact> listOfContacts = new ArrayList<Contact>();
+	private List<Contact> listOfContacts;
+	
+	public AddressBook() {
+		listOfContacts = new ArrayList<Contact>();
+	}
 
 	public List<Contact> getListOfContacts() {
 		return listOfContacts;
-	}
-
-	public String createContact(Contact contact) {
-		listOfContacts.add(contact);
-		return "CREATED";
 	}
 	
 }
