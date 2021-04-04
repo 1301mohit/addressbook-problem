71dbd45662b9fb448cf62fc5ba93072a5bb5c9bd
mohit <mohitkr1301>
2021-04-04 09:06:48 +0530

    Called edit contact method

--- src/main/java/io/addressbookproblem/AddressBookMain.java
+++ src/main/java/io/addressbookproblem/AddressBookMain.java
@@ -5,11 +5,18 @@ public class AddressBookMain
 {
     public static void main( String[] args )
     {
+    	String response;
         System.out.println( "Welcome to Address Book Program" );
         AddressBook addressBook = new AddressBook();
         Contact contact1 = new Contact("Mohit", "Kumar", "Mumbai", "Mumbai","Maharastra",
         							   "123456", "9999999999", "mohit@gmail.com");
-        addressBook.addContact(contact1);
+        response = addressBook.addContact(contact1);
+        System.out.println(response);
+        System.out.println(addressBook.getListOfContacts());
+        Contact contact2 = new Contact("Amit", "Kumar", "Mumbai", "Mumbai","Maharastra",
+				   "123456", "9999999999", "mohit@gmail.com");
+        response = addressBook.editContact("9999999999", contact2);
+        System.out.println(response);
         System.out.println(addressBook.getListOfContacts());
     }
 }
